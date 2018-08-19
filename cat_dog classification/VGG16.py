47fc15a881f2eaaa57fd9c50eff30ff70af0a670
uidd <boss63081@gmail.com>
2018-08-20 01:55:29 +0800

    Update VGG16.py

--- cat_dog classification/VGG16.py
+++ cat_dog classification/VGG16.py
@@ -47,7 +47,7 @@ def load_history(fn):
     return history
 # # Reference
 # dimensions of our images.
-img_width, img_height = 227, 227
+img_width, img_height = 224, 224
 
 train_data_dir = './data/train'
 validation_data_dir = './data/validation'
