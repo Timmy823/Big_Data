734c74e8c76c59b557020ac1b930f9b758fcab46
uidd <boss63081@gmail.com>
2018-08-20 01:26:57 +0800

    Update and rename CNN_VGG16.py to cat_dog classification/VGG16.py

--- cat_dog classification/VGG16.py
+++ cat_dog classification/VGG16.py
@@ -27,65 +27,13 @@ from keras.applications import VGG16
 from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import EarlyStopping
 from keras import backend as K
-#from vis.utils import utils
 from keras import activations
-#from vis.visualization import visualize_activation, get_num_filters
-#from vis.input_modifiers import Jitter
 
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 from keras.layers.normalization import BatchNormalization
 
 
 # # Functions
-def smooth_curve(points, factor=0.8):
-    smoothed = []
-    for point in points:
-        if smoothed:
-            previous = smoothed[-1]
-            smoothed.append(previous * factor + point * (1 - factor))
-        else:
-            smoothed.append(point)
-    return smoothed
-
-def plot_compare(history, steps=-1):
-    if steps < 0:
-        steps = len(history.history['acc'])
-    acc = smooth_curve(history.history['acc'][:steps])
-    val_acc = smooth_curve(history.history['val_acc'][:steps])
-    loss = smooth_curve(history.history['loss'][:steps])
-    val_loss = smooth_curve(history.history['val_loss'][:steps])
-    
-    plt.figure(figsize=(6, 4))
-    plt.plot(loss, c='#0c7cba', label='Train Loss')
-    plt.plot(val_loss, c='#0f9d58', label='Val Loss')
-    plt.xticks(range(0, len(loss), 5))
-    plt.xlim(0, len(loss))
-    plt.title('Train Loss: %.3f, Val Loss: %.3f' % (loss[-1], val_loss[-1]), fontsize=12)
-    plt.legend()
-    
-    plt.figure(figsize=(6, 4))
-    plt.plot(acc, c='#0c7cba', label='Train Acc')
-    plt.plot(val_acc, c='#0f9d58', label='Val Acc')
-    plt.xticks(range(0, len(acc), 5))
-    plt.xlim(0, len(acc))
-    plt.title('Train Accuracy: %.3f, Val Accuracy: %.3f' % (acc[-1], val_acc[-1]), fontsize=12)
-    plt.legend()
-    
-def deprocess_image(x):
-    # normalize tensor: center on 0., ensure std is 0.1
-    x -= x.mean()
-    x /= (x.std() + 1e-5)
-    x *= 0.1
-
-    # clip to [0, 1]
-    x += 0.5
-    x = np.clip(x, 0, 1)
-
-    # convert to RGB array
-    x *= 255
-    x = np.clip(x, 0, 255).astype('uint8')
-    return x
- 
 def save_history(history, fn):
     with open(fn, 'wb') as fw:
         pickle.dump(history.history, fw, protocol=2)
@@ -97,19 +45,6 @@ def load_history(fn):
     with open(fn, 'rb') as fr:
         history.history = pickle.load(fr)
     return history
-
-def jitter(img, amount=32):
-    ox, oy = np.random.randint(-amount, amount+1, 2)
-    return np.roll(np.roll(img, ox, -1), oy, -2), ox, oy
-
-def reverse_jitter(img, ox, oy):
-    return np.roll(np.roll(img, -ox, -1), -oy, -2)
-
-def plot_image(img):
-    plt.figure(figsize=(6, 6))
-    plt.imshow(img)
-    plt.axis('off')
-
 # # Reference
 # dimensions of our images.
 img_width, img_height = 227, 227
@@ -117,16 +52,8 @@ img_width, img_height = 227, 227
 train_data_dir = './data/train'
 validation_data_dir = './data/validation'
 
-nb_train_samples = 2000
-nb_validation_samples = 1000
-epochs = 50
-batch_size = 32
-
-
 # # Data Generator
-
 # #### Keras針對圖片數量不夠多的問題，也提供了解法：利用ImageDataGenerator，我們可以利用一張圖片，進行若干運算之後，得到不同的圖片。
-
 # this is the augmentation configuration we will use for training
 train_datagen = ImageDataGenerator(
     rescale=1. / 255,
@@ -154,7 +81,6 @@ validation_generator = test_datagen.flow_from_directory(
 # # Building a Convolutional Neural Network
 
 # ### VGG-16
-
 # 判斷RGB是在矩陣中的第幾個元素?
 if K.image_data_format() == 'channels_first':
     input_shape = (3, img_width, img_height)
